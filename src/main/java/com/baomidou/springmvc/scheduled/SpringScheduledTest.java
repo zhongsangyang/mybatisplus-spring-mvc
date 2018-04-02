9126e9d16aa53a950501826b9dfce4a1251d28cf
zhongsangyang <1850148339@qq.com>
2018-04-02 20:15:57 +0800

    first commit

---
+++ src/main/java/com/baomidou/springmvc/scheduled/SpringScheduledTest.java
@@ -0,0 +1,19 @@
+package com.baomidou.springmvc.scheduled;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class SpringScheduledTest {
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void schTest1() {
+        Date date = new Date();
+        SimpleDateFormat sim = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateStr = sim.format(date);
+        System.out.println("这是spring定时器1，每五秒执行一次,当前时间：" + dateStr);
+    }
+
+}
---
+++ src/test/java/test/NowTimeTest.java
@@ -0,0 +1,76 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class NowTimeTest {
+    private final static Logger logger =  LoggerFactory.getLogger(NowTimeTest.class);
+    @Test
+    public void testNowTTime(){
+        String s1="";
+        Calendar calendar=Calendar.getInstance();
+        int istr[]=new int[]{
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DATE),
+                calendar.get(Calendar.HOUR),
+                calendar.get(Calendar.SECOND),
+                calendar.get(Calendar.MINUTE),
+                calendar.get(Calendar.MILLISECOND),
+        };
+
+        for(int i=0;i<istr.length;i++){
+            if(i==2||i==4){
+                s1+=istr[i]+" ";
+            }else if(i==istr.length-1){
+                s1+=istr[i];
+            }else if(i==3){
+                s1+=istr[i]+":";
+            }else {
+                s1 += istr[i] + "/";
+            }
+        }
+        System.out.println(s1);
+    }
+    @Test
+    public void otherGetCurrentTime(){
+        Date date = new Date();
+
+        String year = String.format("%tY", date);
+
+        String month = String.format("%tB", date);
+
+        String day = String.format("%te", date);
+
+        System.out.println("今天是："+year+"-"+month+"-"+day+"号");
+    }
+    @Test
+    public void otherGetCurrentTime2(){
+        Date day=new Date();
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        System.out.println(df.format(day));
+
+//        通过Date类来获取当前时间
+
+    }
+
+    @Test
+    public void otherMethod() {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(df.format(System.currentTimeMillis()));
+
+    }
+
+    @Test
+    public void logbackDataBaseInfoTest() {
+        logger.info("logbackDataBaseInfoTest函数运行了","请到数据库里面查看数据吧");
+        System.out.println("运行结束了");
+    }
+}
